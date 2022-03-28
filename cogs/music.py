f1313eacdabe11888354da815fbb9825ba0889cb
SkinnyDevi <xthesmartdartx@gmail.com>
2022-03-28 23:47:55 +0100

    debugging

--- cogs/music.py
+++ cogs/music.py
@@ -94,7 +94,7 @@ class music_cog(commands.Cog):
                 ydl.cache.remove()
                 info = ydl.extract_info(query, download=False)
                 print("OUTT: " + info)
-                inf0 = info['entries'][0]
+                info = info['entries'][0]
             except Exception as error:
                 raise Exception(
                     "There was an error trying to find the specified youtube video: " + str(error))
