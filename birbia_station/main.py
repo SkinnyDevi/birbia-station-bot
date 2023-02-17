9acf68ce7d1deeee7fa66f564b473177f238de16
SkinnyDevi <xthesmartdartx@gmail.com>
2023-02-17 16:42:55 +0000

    Disabled beta

--- birbia_station/main.py
+++ birbia_station/main.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 from .cogs.music import music_cog
 from .cogs.help import help_cog
 
-isDev = True
+isDev = False
 prefix = "birbia-beta " if isDev else "birbia "
 bot = commands.Bot(command_prefix=prefix,
                    intents=discord.Intents.all())
